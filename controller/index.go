094fe332181ac0db67548eb9266708dc9bc334df
Vian <cocatrip@proton.me>
2022-11-13 11:53:31 +0700

    fix: lambda error remove user agent detection

--- controller/index.go
+++ controller/index.go
@@ -1,8 +1,6 @@
 package controller
 
 import (
-	"bytes"
-	"html/template"
 	"log"
 	"net/http"
 
@@ -15,28 +13,7 @@ func GetIndex(c *gin.Context) {
 		log.Fatal(err)
 	}
 
-	if isHtmlOutput(c.Request.Header["User-Agent"][0]) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"Title":     config.Title,
-			"Quote":     config.Quote,
-			"Logo":      config.Logo,
-			"Name":      config.Name,
-			"Alias":     config.Alias,
-			"Desc":      config.Desc,
-			"TechStack": config.TechStack,
-			"Certs":     config.Certs,
-			"SocialMedia":     config.SocialMedia,
-		})
-	} else {
-		t, err := template.ParseFiles("./templates/index.md")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var buffer bytes.Buffer
-		err = t.Execute(&buffer, config)
-		output := buffer.String()
-
-		c.String(http.StatusOK, output)
-	}
+  c.HTML(http.StatusOK, "index.html", gin.H{
+    "Config": config,
+  })
 }
